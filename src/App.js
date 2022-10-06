5e8becf3cacb52e7fe74140fe101bc6d1a36e9a5
root <root@5521694.lesfleursdelanuit.tech>
2022-10-06 03:05:10 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
+import bottomBorder from "./bottomBorder.jpeg";
+import topBorder from "./topBorder.jpeg";
+import unicorn from "./unicorn.jpeg";
+import transparentUnicorn from "./transparentUnicorn.png";
+import transparentBg from "./transparentBg.png";
+import rainbowBorder from "./rainbowBorder.png";
 import './App.css';
+import "./mediaQueries.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="app-mini-header">.</header>
+      <div className="logo-container">
+        <div className="logo-image"></div>
+        <h1>Olivia's Games</h1>
+        <h2>magical games for kids</h2>
+      </div>
     </div>
   );
 }
